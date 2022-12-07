4ff2c102853e21c1772e6589c8e85524316584fe
kaznak <kaznak.at.work@ipebble.org>
2022-12-08 00:33:25 +0900

    use tailwindcss-radix

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,10 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-radix")({
+      // Default: `radix`
+      variantPrefix: "rdx",
+    }),
+  ],
 }
