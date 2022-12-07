1c22108e0785c82447f84280ef9fab9477b83741
Kazutaka Nakamura <kaznak.at.work@ipebble.org>
2022-12-07 14:58:11 +0900

    radix example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+import * as Popover from '@radix-ui/react-popover';
+
+const Home = () => (
+  <div className="bg-violet-400 h-screen w-screen p-10">
+    <h1 className="text-center text-white text-3xl font-bold">
+      Hello Next, Radix and Tailwind!
+    </h1>  
+    <Popover.Root>
+      <Popover.Trigger className="rounded p-2 bg-white text-violet-900 hover:bg-violet-100">Show info</Popover.Trigger>
+      <Popover.Portal>
+        <Popover.Content className="p-2 rounded bg-white text-violet-900">
+          Some more info…
+          <Popover.Arrow className="fill-white" />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
+  </div>
+);
+
+export default Home;
